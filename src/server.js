67320f392cc811d26b17f909ee0dfd2c18b26a9c
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-19 20:24:51 -0500

    Feat: pre run project
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const { errors } = require('celebrate');
+const mongoose = require('mongoose');
+
+const { db: { url }, port} = require('./config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(morgan('dev'));
+
+app.use(errors())
+
+console.log('config ->', url, port)
+
+app.listen(8000, () => {
+  console.log(`Server start on port ${8000}`)
+})
