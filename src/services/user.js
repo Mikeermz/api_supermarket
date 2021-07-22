23925ffa5a26fe5b193fe6d6dc9497d88c23ea24
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-21 21:02:22 -0500

    feat: find user by id
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

--- src/services/user.js
+++ src/services/user.js
@@ -16,7 +16,7 @@ const findUserByEmail = async (email) => {
 
 const findAllUsers = async () => {
   try {
-    const users = await User.find().exec();
+    const users = await User.find({ },'-password').exec();
 
     if (!users) throw ({error: new Error('Users not found'), messsage: 'Users not found'});
 
@@ -26,8 +26,21 @@ const findAllUsers = async () => {
   }
 }
 
+const findUserById = async (id) => {
+  try {
+    const user = await User.findById(id, '-password').exec()
+
+    if (!user) throw ({error: new Error('User not found'), messsage: 'Error Credentials'});
+    
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   createUser,
   findUserByEmail,
-  findAllUsers
+  findAllUsers,
+  findUserById
 };
