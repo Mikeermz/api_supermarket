bff8774c4f4b285d156a21b50700b1f40fe9f2d2
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-21 20:28:59 -0500

    feat: adding middleware and use in userroutes
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

---
+++ src/middlewares/verify.js
@@ -0,0 +1,29 @@
+const JWT = require('jsonwebtoken');
+const { SECRET_KEY } = require('../constants/config')
+const { userService } = require('../services')
+
+
+module.exports = async (req, res, next) => {
+	const AUTH = req.get('Authorization');
+	try {
+		if (AUTH) {
+			const token = AUTH.replace('SPM ', "");
+			console.log(token)
+			const payload = JWT.verify(token, SECRET_KEY);
+			console.log(payload)
+			if (payload) {
+				const { email } = payload;
+				console.log(email)
+				const user =  await userService.findUserByEmail(email);
+
+				req.user = user;
+				next();
+			}
+		} else {
+			throw ({error: new Error('Header not found'), messsage: 'Header not found'});
+		}
+	} catch (error) {
+			console.log(error);
+			res.status(400).json(error);
+	}
+}
