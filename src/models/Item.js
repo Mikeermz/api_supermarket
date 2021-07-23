672fbc2c470fd1a580ef221aa9a195ac4d2dc1c5
Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>
2021-07-22 20:08:16 -0500

    feat adding item model
    Signed-off-by: Mikee Ramirez <mikee@Mikees-MacBook-Pro.local>

---
+++ src/models/Item.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+  name: {
+    type: String, 
+    required: true
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  stock: {
+    type: Number, 
+    default: 1
+  },
+  is_active: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = { Item };
